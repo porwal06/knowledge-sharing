353b0f78dc8c4fefdb013bea3a586dcec4f93035
Deepak Porwal <deepak.porwal@impelsys.com>
2019-05-02 14:45:49 +0530

    Added react basic example

---
+++ ReactJs-basic/examples/basic-example/src/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+//Example 1
+/*function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}*/
+
+// Example 2
+
+function FormattedDate(props) {
+   return (<div>
+     <h2>It is {props.date.toLocaleTimeString()}.</h2>
+     <input type="text" />
+   </div>);
+  // return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <FormattedDate date={this.state.date} />
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <Clock />
+    </div>
+  );
+}
+export default App;
+
